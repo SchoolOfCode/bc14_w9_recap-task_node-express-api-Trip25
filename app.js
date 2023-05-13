e71792fb8db51771853cb865bf9ef184935be3e2
Trip25 <t.gurung1995@gmail.com>
2023-05-13 18:29:02 +0100

    GET all users done- will need to reformat the payload data

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
 import express from "express";
 
+//import all the functions from the users.js file
+import {
+  getUsers,
+  getUserByID,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
+} from "./users.js";
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.json({
-    status: true,
-    payload: "This route works!",
-  });
+// app.use("/", (req, res) => {
+//   res.json({
+//     status: true,
+//     payload: "This route works!",
+//   });
+// });
+
+// Custom middleware
+// app.use((req, res, next) => {
+//   console.log(`[${new Date().toISOString()}] ${req.method} to ${req.path}`);
+//   next(); // Don't forget to call next()!
+// });
+// app.use((req, res, next) => {
+//   const date = new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' });
+//   console.log(`[${date}] ${req.method} to ${req.path}`);
+//   next(); // Don't forget to call next()!
+// });
+
+//GET all users
+app.get("/api/users", async (req, res) => {
+  let users = await getUsers();
+  res.json(users)
+  console.log(users);
+  
+  //send data in required format
+  // res.send({
+  //   success: true, //this indicates that the request was successful
+  //   payload: users, //this is the data returned by the request
+  // })
 });
+
+//GET user by ID
+app.get("/api/users/:id", async (req, res) => {
+  // const users = await getUsers();
+  // const theUser = users.find((user) => user.id === req.params.id);
+  // if (theUser) {
+  //   res.send({
+  //     success: true,
+  //     payload: theUser,
+  //   });
+  // } else {
+  //   res.status(404).send({
+  //     success: false,
+  //     message: "Hmm we don't seem to have a user with that ID",
+  //   });
+  // }
+});
+
+//POST aka create a new user
+
+
+//PATCH aka update a user by ID
+
+
+//DELETE aka delete a user by ID
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- users.js
+++ users.js
@@ -3,12 +3,20 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "users.json";
 
-export async function getUsers() {}
-
+//GET all users
+export async function getUsers() {
+    let data = await fs.readFile(fileName, "utf-8");
+    let users = JSON.parse(data);
+    return users;
+}
+//Get user by ID
 export async function getUserByID(id) {}
 
+//Create a new user
 export async function createUser(newUser) {}
 
+//Update a user by ID
 export async function updateUserByID(id, updatedUser) {}
 
+//Delete a user by ID
 export async function deleteUserByID(id) {}
