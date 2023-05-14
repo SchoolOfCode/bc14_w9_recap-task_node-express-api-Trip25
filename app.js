a2b2c20b76881193311868c54e00e015a9f2d704
Trip25 <t.gurung1995@gmail.com>
2023-05-14 21:45:13 +0100

    added public folder and html- to work on linking and functionality to front

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ import {
 const app = express();
 const port = 3000;
 
+//for static files
+app.use(express.static("public"));
 app.use(express.json());
+
 
 // app.use("/", (req, res) => {
 //   res.json({
---
+++ public/main.js
@@ -0,0 +1,13 @@
+const url = "http://localhost:3000";
+
+//assign variables and query selectors
+
+
+
+
+
+
+
+
+
+
